4be5a31769d8bf7fcf791628ac20bc7984ce9f8e
Stef-00012 <me@stefdp.com>
2026-04-15 16:32:44 +0200

    [build] add NDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,9 @@ android {
         applicationId = "com.stefdp.zipline"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
+        versionCode = 25
         versionName = "2.0.0"
+        ndkVersion = "29.0.14206865"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

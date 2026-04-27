feea7c7b8713f96210e8be6962a099080eda606f
Stef-00012 <me@stefdp.com>
2026-04-27 18:51:49 +0200

    [build] fix crash on export files setting open

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.stefdp.zipline"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "2.1.3"
+        versionCode = 36
+        versionName = "2.1.4"
         ndkVersion = "29.0.14206865"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
